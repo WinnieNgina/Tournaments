1c5d6828aefca4c0ab0608c7e1c20ea37cd92ddc
Winfred <winfrednginakilonzo@gmail.com>
2024-02-20 23:12:40 +0300

    Implemented lazy loading for some navigation properties

--- Tournaments/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Tournaments/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6d605f7a46767961d9db2604bc3e85e4a32a97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d20e15a684f95325b128dbc8b56ba9524bacfc7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
