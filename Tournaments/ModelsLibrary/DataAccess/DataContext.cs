826dad1f3c6260c2121e5e427e36de6e1f421c9f
Winfred <winfrednginakilonzo@gmail.com>
2024-02-20 09:22:36 +0300

    Fixed a bug on the datacontext class

--- Tournaments/ModelsLibrary/DataAccess/DataContext.cs
+++ Tournaments/ModelsLibrary/DataAccess/DataContext.cs
@@ -14,7 +14,7 @@ namespace ModelsLibrary.DataAccess
         public DbSet<CoachModel> Coach { get; set; }
         public DbSet<MatchUpEntryModel> Entries { get; set; }
         public DbSet<MatchUpModel> MatchUp { get; set; }
-        public DbSet<NextOfKinModel> NextOfKin { get; set;}
+        public DbSet<NextOfKinModel> NextOfKin { get; set; }
         public DbSet<ParticipationModel> Participation { get; set; }
         public DbSet<PlayerModel> Player { get; set; }
         public DbSet<PrizeModel> Prize { get; set; }
@@ -23,14 +23,20 @@ namespace ModelsLibrary.DataAccess
         public DbSet<TournamentModel> Tournament { get; set; }
         public DbSet<TournamentOrganizerModel> Organizer { get; set; }
         public DbSet<TournamentPrizeModel> TournamentPrizes { get; set; }
-    }
-    private static void SeedRoles(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<IdentityRole>().HasData
-            (
-            new IdentityRole() { Name = RoleNames.Player, ConcurrencyStamp = Guid.NewGuid().ToString(), NormalizedName = "PLAYER" },
-            new IdentityRole() { Name = RoleNames.Organizer, ConcurrencyStamp = Guid.NewGuid().ToString(), NormalizedName = "ORGANIZER" },
-            new IdentityRole() { Name = RoleNames.Coach, ConcurrencyStamp = Guid.NewGuid().ToString(), NormalizedName = "COACH" }
-            );
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SeedRoles(modelBuilder);
+        }
+
+        private static void SeedRoles(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityRole>().HasData
+                (
+                new IdentityRole() { Name = RoleNames.Player, ConcurrencyStamp = Guid.NewGuid().ToString(), NormalizedName = "PLAYER" },
+                new IdentityRole() { Name = RoleNames.Organizer, ConcurrencyStamp = Guid.NewGuid().ToString(), NormalizedName = "ORGANIZER" },
+                new IdentityRole() { Name = RoleNames.Coach, ConcurrencyStamp = Guid.NewGuid().ToString(), NormalizedName = "COACH" }
+                );
+        }
     }
 }
