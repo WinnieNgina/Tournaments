e97440887d4942ec26134279d14bc543463393f2
Winfred <winfrednginakilonzo@gmail.com>
2024-02-26 00:16:22 +0300

    Update login dto to ensure one gets either username or email

--- Tournaments/API/DTO/LoginDTO.cs
+++ Tournaments/API/DTO/LoginDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTO
 {
@@ -7,12 +8,23 @@ namespace API.DTO
         [EmailAddress]
         [Display(Name = "Email Address")]
         public string Email { get; set; }
-        
+
         [Display(Name = "User Name")]
         public string UserName { get; set; }
+
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
+
+        // Custom validation to ensure that either Email or UserName is provided
+        [Required(ErrorMessage = "Either Email or User Name is required.")]
+        public string LoginIdentifier
+        {
+            get
+            {
+                return Email ?? UserName;
+            }
+        }
     }
 }
--- Tournaments/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Tournaments/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011bb605bb3a05fdf9abae463786195ce2a4f55b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f93e7f9a69862e1a3d23521b4609689757d81bf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
